3664b7d90be35cbdc485f6ee5cb7f42af9ae71f5
wiseac <89418908+wiseac@users.noreply.github.com>
2022-07-20 13:38:12 -0400

    Adafruit

---
+++ Firmware/Adafruit AQI (CP7)/secrets.py
@@ -0,0 +1,12 @@
+secrets = {
+    'ssid' : '',
+    'password' : '',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'github_token' : '',
+    'hackaday_token' : '',
+    'aio_user' : '',
+    'aio_key' : '',
+    'latitude': , # https://www.latlong.net/
+    'longitude': ,
+    'elevation': ,
+    }
